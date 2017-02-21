b4f8e88f77ce84df5423bdc5a7306fa42bb90f39
Taylor Pechacek <taylortpechacek@gmail.com>
2017-02-21 21:58:26 +1100

    back on track

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-// Create a new component. This component should product
-// some HTML
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyC6M676EDOZu2Kl6qm6OIIGP0lnEdurRB4';
 
-const App = function() {
-	return <div>Hi!</div>;
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+			// same thing as videos.videos
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
---
+++ todo.md
@@ -0,0 +1,6 @@
+# Things to look into
+- ES6 syntax
+- fat arrow functions
+- functional component vs. class based component. functional is super simple, then a class component is more complex. Has to have a render() function.
+
+state is a plan javascript object used to record and react to user events.
